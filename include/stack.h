056d81506fe613df60abbb995b73ee96529e307f
Jan Felix Langenbach <o.hase3@gmail.com>
2019-08-06 16:16:31 +0200

    Implement Stack methods.

--- include/stack.h
+++ include/stack.h
@@ -21,7 +21,7 @@ JFLCSTD_PUBLIC typedef struct Stack {
     size_t length_;
 } Stack;
 
-JFLCSTD_PUBLIC Stack **jfl_Stack_new();
+JFLCSTD_PUBLIC Stack *jfl_Stack_new();
 JFLCSTD_PUBLIC int jfl_Stack_push(Stack **stack, void *data);
 JFLCSTD_PUBLIC void *jfl_Stack_pop(Stack **stack);
 JFLCSTD_PUBLIC size_t jfl_Stack_length(Stack **stack);
--- src/stack.c
+++ src/stack.c
@@ -1,7 +1,47 @@
 #include "stack.h"
 
-Stack **jfl_Stack_new();
-int jfl_Stack_push(Stack **stack, void *data);
-void *jfl_Stack_pop(Stack **stack);
-size_t jfl_Stack_length(Stack **stack);
-void jfl_Stack_free(Stack **stack);
+#include <stdlib.h>
+
+Stack *jfl_Stack_new() { return NULL; }
+
+int jfl_Stack_push(Stack **stack, void *data) {
+    Stack *new_block = malloc(sizeof(Stack));
+    if (!new_block) return 1;
+    new_block->data = data;
+    new_block->next = *stack;
+    new_block->length_ = jfl_Stack_length(stack) + 1;
+    *stack = new_block;
+    return 0;
+}
+
+void *jfl_Stack_pop(Stack **stack) {
+    Stack *popped = *stack;
+    if (!popped) return NULL;
+    void *data = popped->data;
+    *stack = popped->next;
+    free(popped);
+    return data;
+}
+
+size_t jfl_Stack_length(Stack **stack) {
+    if (!*stack) return 0;
+    return (*stack)->length_;
+}
+
+void jfl_Stack_free(Stack **stack) {
+    if (!stack) return;
+    if (!*stack) return;
+    jfl_Stack_free(&((*stack)->next));
+    free(*stack);
+}
+
+void jfl_Stack_free_iter(Stack **stack) {
+    if (!stack) return;
+    Stack *current = *stack;
+    Stack *next;
+    while (current) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
