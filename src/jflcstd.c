f8a2453b88bbb3b47d18cd8c9d574785f3c93f90
Jan Felix Langenbach <o.hase3@gmail.com>
2019-08-06 18:29:31 +0200

    Remove meson example code.

--- src/jflcstd.c
+++
@@ -1,8 +0,0 @@
-#include "jflcstd.h"
-
-/* This function will not be exported and is not
- * directly callable by users of this library.
- */
-int internal_function() { return 0; }
-
-int jfl_func() { return internal_function(); }
